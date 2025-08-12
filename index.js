faa720a510ae365f34d43087f57a19a4d644cdae
mangalam-srv <mangalamstp2005@gmail.com>
2025-08-12 23:18:58 +0530

    first

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const express = require("express");
-const app = express();
+import dotenv from "dotenv"
+import { app } from "./app.js";
 
+
+import express from "express";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./.env'
+})//this does the work of importing the variabbles declared in the .envfile 
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||4000 ,()=>{
+        console.log(`server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection Failed",err);
+    
+})
 
 
